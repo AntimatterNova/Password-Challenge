cde1c7605e60767e54b1ba817b4fc0acb33ffcaf
AntimatterNova <dmvsecondary@gmail.com>
2023-04-05 01:30:08 -0500

    Finished the JavaScript system for genereating the password

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,9 @@ function writePassword() {
 }
 
 generateBtn.addEventListener("click", writePassword);
+
 function generatePassword() {
-  //Defines lists of usable characters and base password length
+  // Defines lists of usable characters and base password length
   var passLength = 0;
   var hasSpecial = false;
   var hasNumbers = false;
@@ -23,15 +24,18 @@ function generatePassword() {
   var lowerChars = "abcdefghijklmnopqrstuvwxyz";
   var upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
+  // Asks the user how long they want the password to be
   while (passLength < 8 || passLength > 128) {
     passLength = parseInt(prompt("Choose a password length between 8 and 128 characters?"));
   };
 
+  // Checks of the user's input is a valid number, or within the allowed length
   if (isNaN(passLength)) {
     alert("The number you choose must be between 8 and 128.");
     return "";
   };
   
+  // Ask the user what types of symbols they want in the output
   while (!hasLowercase && !hasUppercase && !hasNumbers && !hasSpecial) {
     hasSpecial = confirm("Should the password contain symbols");
     hasNumbers = confirm("Should the password contain numbers?");
@@ -39,8 +43,37 @@ function generatePassword() {
     hasUppercase = confirm("Should the password contain uppercase letters?");
   };
   
+  // If the user does not select at least one of the output types, it will prompt them to choose one
   if (!(hasLowercase || hasUppercase || hasNumbers || hasSpecial)) {
     alert("Please select at least one special character.");
     return "";
   };
+
+  let availableChars = "";
+  if (hasLowercase) {
+    availableChars += lowerChars;
+  };
+  if (hasUppercase) {
+    availableChars += upperChars;
+  };
+  if (hasNumbers) {
+    availableChars += numChars;
+  };
+  if (hasSpecial) {
+    availableChars += specChars;
+  };
+
+  // This is the function that generates a randomized password
+  let password = "";
+  for (let i = 0; i < passLength; i++) {
+    password += availableChars.charAt(Math.floor(Math.random() * availableChars.length));
+  };
+
+  return password;
 };
+
+// This function pushes the generated password to the html
+document.getElementById("generate-password-btn").addEventListener("click", function() {
+  var password = generatePassword();
+  alert("Your new password is: " + password);
+});
